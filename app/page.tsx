8915f700e44584c033893cf090687d0f2bec77b0
touraye <touraye07@gmail>
2024-06-26 18:52:47 +0000

    handle login user state

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,21 @@
-import { currentUser } from "@clerk/nextjs/server";
-import Guest from "@/components/Guest";
+import { currentUser } from '@clerk/nextjs/server'
+import Guest from '@/components/Guest'
+import AddTransaction from '@/components/AddTransaction'
+import Balance from '@/components/Balance'
 
 const HomePage = async () => {
-  const user = await currentUser();
-  if (!user) {
-  return <Guest />
+	const user = await currentUser()
+	if (!user) {
+		return <Guest />
+	}
+
+	return (
+		<main>
+			<h3>Welcome, {user.firstName}</h3>
+			<Balance />
+			<AddTransaction />
+		</main>
+	)
 }
 
-  return (<main>
-    <h2>Welcome, {user.firstName}</h2>
-  </main> );
-}
- 
-export default HomePage;
+export default HomePage
