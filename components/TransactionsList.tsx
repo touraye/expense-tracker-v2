da972577e676704a81063ee2141d5dce38924cc3
touraye <touraye07@gmail>
2024-06-28 12:05:55 +0000

    transaction list

---
+++ components/TransactionsList.tsx
@@ -0,0 +1,29 @@
+import TransactionItem from "./TransactionItem";
+import getTransaction from "@/app/actions/getTransactions";
+import { Transaction } from "@/types/Transaction";
+
+const AddTransactionList = async () => {
+    const { transactions ,error } = await getTransaction();
+    
+    if (error) {
+        return <p className="error">{error}</p>
+    }
+
+    return (<>
+        <h4>History</h4>
+        <ul className="list">
+            {
+                transactions &&
+                transactions.map((transaction: Transaction) => (
+                    <TransactionItem
+                        key={transaction.id}
+                        transaction={transaction}
+                    />
+                ))
+            }
+        </ul>
+
+    </>);
+}
+ 
+export default AddTransactionList;
