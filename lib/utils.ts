4077f7dbf2c0e08783176caa46b1e7cef2bcedb2
touraye <touraye07@gmail>
2024-06-28 11:55:59 +0000

    fix currency format

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,3 @@
 export function currentFormatter(x: number): string {
-    return x.toString().replace(/\B(?=(\d{3})+(?!d))/g, ',');
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
